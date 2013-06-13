f5bcc81414f18df25dfc3fdf8f001fcfc71730d5
Sergey Lipskiy <gonetz@ngs.ru>
2013-06-13 16:57:07 +0700

    Correct color buffer height calculation.

--- gDP.cpp
+++ gDP.cpp
@@ -348,12 +348,14 @@ void gDPSetColorImage( u32 format, u32 size, u32 width, u32 address )
 
 	if (gDP.colorImage.address != address || gDP.colorImage.width != width)
 	{
-		if (width >= gDP.scissor.lrx-1 && width <= gDP.scissor.lrx+1)
+		if (width == VI.width)
+			gDP.colorImage.height = max(VI.height, gSP.viewport.height);
+		else if (width == gDP.scissor.lrx && width == gSP.viewport.width)
+			gDP.colorImage.height = max(gDP.scissor.lry, gSP.viewport.height);
+		else if (width == gDP.scissor.lrx)
 			gDP.colorImage.height = gDP.scissor.lry;
-		else if (width >= gSP.viewport.width-1 && width <= gSP.viewport.width+1)
-			gDP.colorImage.height = gSP.viewport.height;
 		else
-			gDP.colorImage.height = 1;
+			gDP.colorImage.height = gSP.viewport.height;
 
 		if (OGL.frameBufferTextures)// && address != gDP.depthImageAddress)
 		{
