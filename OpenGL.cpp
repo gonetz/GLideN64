15e5947503e1dab7f855f2df80d7da89e402245d
Sergey Lipskiy <gonetz@ngs.ru>
2013-11-23 14:44:11 +0700

    Correct condition for depth buffer rendering.

--- OpenGL.cpp
+++ OpenGL.cpp
@@ -910,7 +910,7 @@ void OGL_DrawTexturedRect( float ulx, float uly, float lrx, float lry, float uls
 	OGL_UpdateStates();
 
 //	if ((gDP.otherMode.l >> 16) == 0x3c18 && gDP.combine.muxs0 == 0x00ffffff && gDP.combine.muxs1 == 0xfffff238) //depth image based fog
-	if (gDP.textureImage.address >= gDP.depthImageAddress &&  gDP.textureImage.address < (gDP.depthImageAddress +  gDP.colorImage.width*gDP.colorImage.width*6/4))
+	if (gSP.textureTile[0]->frameBuffer == NULL && gSP.textureTile[1]->frameBuffer == NULL && gDP.textureImage.address >= gDP.depthImageAddress &&  gDP.textureImage.address < (gDP.depthImageAddress +  gDP.colorImage.width*gDP.colorImage.width*6/4))
 		GLS_SetShadowMapCombiner();
 
 	glDisable( GL_CULL_FACE );
