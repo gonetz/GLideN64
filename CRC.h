1a43c89db39f46ce22d3071c859e5e52cc0c41d1
Sergey Lipskiy <gonetz@ngs.ru>
2014-09-03 12:08:04 +0700

    Code cleanup: cleanup CRC.h and CRC.cpp

--- CRC.h
+++ CRC.h
@@ -2,5 +2,5 @@
 
 void CRC_BuildTable();
 
-u32 CRC_Calculate( u32 crc, void *buffer, u32 count );
-u32 CRC_CalculatePalette( u32 crc, void *buffer, u32 count );
+u32 CRC_Calculate( u32 crc, const void *buffer, u32 count );
+u32 CRC_CalculatePalette( u32 crc, const void *buffer, u32 count );
