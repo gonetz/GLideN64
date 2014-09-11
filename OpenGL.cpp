e737cb80fa87d3aa3d7f93874c80cdeb80520a25
Sergey Lipskiy <gonetz@ngs.ru>
2014-09-11 15:23:55 +0700

    Code cleanup: remove unused isExtensionSupported() function.

--- OpenGL.cpp
+++ OpenGL.cpp
@@ -26,36 +26,6 @@
 
 GLInfo OGL;
 
-BOOL isExtensionSupported( const char *extension )
-{
-	const GLubyte *extensions = NULL;
-	const GLubyte *start;
-	GLubyte *where, *terminator;
-
-	where = (GLubyte *) strchr(extension, ' ');
-	if (where || *extension == '\0')
-		return 0;
-
-	extensions = glGetString(GL_EXTENSIONS);
-
-	start = extensions;
-	for (;;)
-	{
-		where = (GLubyte *) strstr((const char *) start, extension);
-		if (!where)
-			break;
-
-		terminator = where + strlen(extension);
-		if (where == start || *(where - 1) == ' ')
-			if (*terminator == ' ' || *terminator == '\0')
-				return TRUE;
-
-		start = terminator;
-	}
-
-	return FALSE;
-}
-
 void OGL_InitExtensions()
 {
 	const char *version = reinterpret_cast<const char*>(glGetString(GL_VERSION));
