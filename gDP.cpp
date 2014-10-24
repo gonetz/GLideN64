72ba52767c910c81850c8d220747600262499b34
Sergey Lipskiy <gonetz@ngs.ru>
2014-10-24 20:49:07 +0700

    Fix depth buffer clear detection in gDPFillRectangle.
    Automobili Lamborghini uses DEPTH_CLEAR_COLOR for pause fillrect color.

--- gDP.cpp
+++ gDP.cpp
@@ -799,7 +799,7 @@ void gDPFillRectangle( s32 ulx, s32 uly, s32 lrx, s32 lry )
 			render.clearDepthBuffer();
 			return;
 		}
-	} else if (gDP.fillColor.color == DEPTH_CLEAR_COLOR) {
+	} else if (gDP.fillColor.color == DEPTH_CLEAR_COLOR && gDP.otherMode.cycleType == G_CYC_FILL) {
 		depthBufferList().saveBuffer(gDP.colorImage.address);
 		gDPFillRDRAM(gDP.colorImage.address, ulx, uly, lrx, lry, gDP.colorImage.width, gDP.colorImage.size, gDP.fillColor.color);
 		render.clearDepthBuffer();
