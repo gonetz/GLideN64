5384e189c45a6238c2957d7c0d829742eee30019
Sergey Lipskiy <gonetz@ngs.ru>
2015-03-11 20:56:16 +0600

    Don't discard frame buffers creation when VI.height == 0
    Fixed auxilary color buffers creation in Pokemon Stadium.

--- FrameBuffer.cpp
+++ FrameBuffer.cpp
@@ -336,7 +336,7 @@ FrameBuffer * FrameBufferList::findTmpBuffer(u32 _address)
 
 void FrameBufferList::saveBuffer(u32 _address, u16 _format, u16 _size, u16 _width, u16 _height, bool _cfb)
 {
-	if (VI.width == 0 || VI.height == 0 || _height == 0) // H width is zero. Don't save
+	if (VI.width == 0 || _height == 0)
 		return;
 	OGLVideo & ogl = video();
 	if (m_pCurrent != NULL && gDP.colorImage.height > 0) {
