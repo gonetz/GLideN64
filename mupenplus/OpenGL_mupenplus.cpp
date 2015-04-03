f0ef6ef31d309c203484d600eb9178536cb6c2eb
Sergey Lipskiy <gonetz@ngs.ru>
2015-04-03 20:51:07 +0600

    Set CHANGED_VIEWPORT when viewport cvhanged in GLVideoMupenPlus::_swapBuffers()

--- mupenplus/OpenGL_mupenplus.cpp
+++ mupenplus/OpenGL_mupenplus.cpp
@@ -94,8 +94,10 @@ void OGLVideoMupenPlus::_swapBuffers()
 	// if emulator defined a render callback function, call it before buffer swap
 	if (renderCallback != NULL) {
 		glUseProgram(0);
-		if (config.frameBufferEmulation.N64DepthCompare == 0)
+		if (config.frameBufferEmulation.N64DepthCompare == 0) {
 			glViewport(0, getHeightOffset(), getScreenWidth(), getScreenHeight());
+			gSP.changed |= CHANGED_VIEWPORT;
+		}
 		gDP.changed |= CHANGED_COMBINE;
 		(*renderCallback)((gSP.changed&CHANGED_CPU_FB_WRITE) == 0 ? 1 : 0);
 	}
