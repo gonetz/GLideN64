f1a252a6d0a7e522fa531885ec1915a6f5a93ab1
Sergey Lipskiy <gonetz@ngs.ru>
2015-04-17 21:03:38 +0600

    Remove posix related code. It is unused legacy code.

--- posix/ZilmarAPIImpl_posix.cpp
+++
@@ -1,9 +0,0 @@
-#include "../winlnxdefs.h"
-#include "../PluginAPI.h"
-#include "../GLideN64.h"
-#include "../OpenGL.h"
-
-void PluginAPI::DllAbout(HWND _hParent)
-{
-	puts( "GLideN64 alpha. Based on Orkin's glN64 v0.4" );
-}
