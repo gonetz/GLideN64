13d2ff4710292c0fac95cbb4034c4bde3c234d89
Sergey Lipskiy <gonetz@ngs.ru>
2015-04-20 16:12:42 +0600

    Move code from ZilmarAPIImpl_common.cpp to ZilmarAPIImpl_windows.cpp Remove ZilmarAPIImpl_common.cpp

--- src/windows/ZilmarAPIImpl_windows.cpp
+++ src/windows/ZilmarAPIImpl_windows.cpp
@@ -2,8 +2,35 @@
 #include "../PluginAPI.h"
 #include "../GLideN64.h"
 #include "../GLideNUI/GLideNUI.h"
+#include "../OpenGL.h"
+#include "../Config.h"
+#include "../Revision.h"
 
 void PluginAPI::DllAbout(/*HWND _hParent*/)
 {
 	RunAbout();
+}
+
+void PluginAPI::CaptureScreen(char * _Directory)
+{
+	video().setCaptureScreen(_Directory);
+}
+
+void PluginAPI::DllConfig(HWND _hParent)
+{
+	Config_DoConfig(/*_hParent*/);
+}
+
+void PluginAPI::GetDllInfo(PLUGIN_INFO * PluginInfo)
+{
+	PluginInfo->Version = 0x103;
+	PluginInfo->Type = PLUGIN_TYPE_GFX;
+	sprintf(PluginInfo->Name, "%s rev.%s", pluginName, PLUGIN_REVISION);
+	PluginInfo->NormalMemory = FALSE;
+	PluginInfo->MemoryBswaped = TRUE;
+}
+
+void PluginAPI::ReadScreen(void **_dest, long *_width, long *_height)
+{
+	video().readScreen(_dest, _width, _height);
 }
