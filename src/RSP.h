e8d8ee93e45e2304d09b312f7bf2eea8b3a1013f
Sergey Lipskiy <gonetz@ngs.ru>
2015-05-05 19:54:18 +0600

    Correct RSP_SegmentToPhysical
    Fixed Glover crash after intro #474

--- src/RSP.h
+++ src/RSP.h
@@ -16,7 +16,7 @@ extern RSPInfo RSP;
 
 extern u32 DepthClearColor;
 
-#define RSP_SegmentToPhysical( segaddr ) ((gSP.segment[(segaddr >> 24) & 0x0F] + (segaddr & 0x00FFFFFF)) & 0x00FFFFFF)
+#define RSP_SegmentToPhysical( segaddr ) ((gSP.segment[(segaddr >> 24) & 0x0F] + (segaddr & RDRAMSize)) & RDRAMSize)
 
 void RSP_Init();
 void RSP_ProcessDList();
