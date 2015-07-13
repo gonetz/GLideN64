dbfbeb1b134471fb2e2f3d4c73f5ce0602c94170
purplemarshmallow <purplemarshmallow@users.noreply.github.com>
2015-07-13 20:27:40 +0200

    fix copy color to RDRAM in LLE
    fixes #554

--- src/gDP.cpp
+++ src/gDP.cpp
@@ -880,6 +880,9 @@ void gDPTextureRectangleFlip( f32 ulx, f32 uly, f32 lrx, f32 lry, s32 tile, f32 
 
 void gDPFullSync()
 {
+	if (config.frameBufferEmulation.copyToRDRAM)
+		FrameBuffer_CopyToRDRAM(gDP.colorImage.address);
+
 	*REG.MI_INTR |= MI_INTR_DP;
 
 	CheckInterrupts();
