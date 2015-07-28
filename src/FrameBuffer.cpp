d9384e8ad8e093a8aed42ef9f0482d0ac5484ba0
purplemarshmallow <purplemarshmallow@users.noreply.github.com>
2015-07-28 16:21:59 +0200

    fix #620

--- src/FrameBuffer.cpp
+++ src/FrameBuffer.cpp
@@ -457,7 +457,7 @@ void FrameBufferList::saveBuffer(u32 _address, u16 _format, u16 _size, u16 _widt
 			if (m_pCurrent->m_width == VI.width)
 				gDP.colorImage.height = min(gDP.colorImage.height, VI.height);
 			m_pCurrent->m_endAddress = min(RDRAMSize, m_pCurrent->m_startAddress + (((m_pCurrent->m_width * gDP.colorImage.height) << m_pCurrent->m_size >> 1) - 1));
-			if (!config.frameBufferEmulation.copyFromRDRAM && !m_pCurrent->_isMarioTennisScoreboard() && !m_pCurrent->m_isDepthBuffer && !m_pCurrent->m_copiedToRdram && !m_pCurrent->m_cfb && gDP.colorImage.height > 1) {
+			if (!m_pCurrent->_isMarioTennisScoreboard() && !m_pCurrent->m_isDepthBuffer && !m_pCurrent->m_copiedToRdram && !m_pCurrent->m_cfb && gDP.colorImage.height > 1) {
 				m_pCurrent->copyRdram();
 			}
 		}
