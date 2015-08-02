8463c0347f21201b1de84324ccdc3f297c61c123
purplemarshmallow <purplemarshmallow@users.noreply.github.com>
2015-08-02 21:01:02 +0200

    fix regression in Mario Tennis

--- src/FrameBuffer.h
+++ src/FrameBuffer.h
@@ -28,6 +28,7 @@ struct FrameBuffer
 	u32 m_size, m_width, m_height, m_fillcolor, m_validityChecked;
 	float m_scaleX, m_scaleY;
 	bool m_copiedToRdram;
+	bool m_fingerprint;
 	bool m_cleared;
 	bool m_changed;
 	bool m_cfb;
