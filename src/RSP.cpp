gonetz/GLideN64

commit 77f342fc630a20b6e3724f4dda5ea4bb02cc4bf4

fix depth buffer copy in LLE
in LLE it works well for Star Wars Episode I - Racer but for most games
it does not work correctly

src/RSP.cpp
@@ -204,10 +204,6 @@ void RSP_ProcessDList()
 			RSP_CheckDLCounter();
 		}
 	}
-
-	if (config.frameBufferEmulation.copyDepthToRDRAM)
-		FrameBuffer_CopyDepthBuffer( gDP.colorImage.address );
-
 	RSP.busy = FALSE;
 	gDP.changed |= CHANGED_COLORBUFFER;
 }

src/gDP.cpp
@@ -882,6 +882,8 @@ void gDPFullSync()
 {
 	if (config.frameBufferEmulation.copyToRDRAM)
 		FrameBuffer_CopyToRDRAM(gDP.colorImage.address);
+	if (config.frameBufferEmulation.copyDepthToRDRAM)
+		FrameBuffer_CopyDepthBuffer(gDP.colorImage.address);
 
 	*REG.MI_INTR |= MI_INTR_DP;
 

commit c6988bb9634e4dec14ff25811a97e6fdfa2342d8

Fig regression with frame/depth buffer copy in HLE mode caused by commit 77f342f.

src/RSP.cpp
@@ -204,6 +204,12 @@ void RSP_ProcessDList()
 			RSP_CheckDLCounter();
 		}
 	}
+
+	if (config.frameBufferEmulation.copyToRDRAM)
+		FrameBuffer_CopyToRDRAM(gDP.colorImage.address);
+	if (config.frameBufferEmulation.copyDepthToRDRAM)
+		FrameBuffer_CopyDepthBuffer(gDP.colorImage.address);
+
 	RSP.busy = FALSE;
 	gDP.changed |= CHANGED_COLORBUFFER;
 }

src/gDP.cpp
@@ -880,10 +880,12 @@ void gDPTextureRectangleFlip( f32 ulx, f32 uly, f32 lrx, f32 lry, s32 tile, f32 
 
 void gDPFullSync()
 {
-	if (config.frameBufferEmulation.copyToRDRAM)
-		FrameBuffer_CopyToRDRAM(gDP.colorImage.address);
-	if (config.frameBufferEmulation.copyDepthToRDRAM)
-		FrameBuffer_CopyDepthBuffer(gDP.colorImage.address);
+	if (RSP.bLLE) {
+		if (config.frameBufferEmulation.copyToRDRAM)
+			FrameBuffer_CopyToRDRAM(gDP.colorImage.address);
+		if (config.frameBufferEmulation.copyDepthToRDRAM)
+			FrameBuffer_CopyDepthBuffer(gDP.colorImage.address);
+	}
 
 	*REG.MI_INTR |= MI_INTR_DP;
 