f59840250c0aae779c77de50d4026866649ae5d2
Sergey Lipskiy <gonetz@ngs.ru>
2015-10-03 19:31:16 +0600

    Enable shaders storage by default.

--- src/Config.cpp
+++ src/Config.cpp
@@ -34,7 +34,7 @@ void Config::resetToDefaults()
 	generalEmulation.enableNoise = 1;
 	generalEmulation.enableHWLighting = 0;
 	generalEmulation.enableCustomSettings = 1;
-	generalEmulation.enableShadersStorage = 0;
+	generalEmulation.enableShadersStorage = 1;
 	generalEmulation.hacks = 0;
 #ifdef ANDROID
 	generalEmulation.forcePolygonOffset = 0;
