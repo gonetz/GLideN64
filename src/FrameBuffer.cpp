c95a6bb82b9f0587fd0e008f196581a34ad6d89a
purplemarshmallow <purplemarshmallow@users.noreply.github.com>
2015-10-23 17:22:04 +0200

    fix bug where the top of the buffer is filled with garbage

--- src/FrameBuffer.cpp
+++ src/FrameBuffer.cpp
@@ -261,7 +261,7 @@ void FrameBuffer::copyRdram()
 	const u32 dataSize = stride * height;
 
 	// Auxiliary frame buffer
-	if (m_width != VI.width) {
+	if (m_width != VI.width && !config.frameBufferEmulation.copyAuxiliary) {
 		// Write small amount of data to the start of the buffer.
 		// This is necessary for auxilary buffers: game can restore content of RDRAM when buffer is not needed anymore
 		// Thus content of RDRAM on moment of buffer creation will be the same as when buffer becomes obsolete.
