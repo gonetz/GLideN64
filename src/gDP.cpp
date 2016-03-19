gonetz/GLideN64

commit f6be5fd39fdcc9fd992087303138f9b2cff7dfe0

Fix bug with line calculation in gDPLoadBlock.
Fixed South Park Rally (All) - Gfx Issue #391
and probably other similar issues.

src/gDP.cpp
@@ -625,9 +625,10 @@ void gDPLoadBlock(u32 tile, u32 uls, u32 ult, u32 lrs, u32 dxt)
 		u32 tmemAddr = gDP.loadTile->tmem;
 
 		if (dxt > 0) {
-			u32 line = (2047 + dxt) / dxt;
-			u32 bpl = line << 3;
-			u32 height = bytes / bpl;
+			const u32 widthInQWords = (bytes >> 3);
+			const u32 height = (widthInQWords * dxt) / 2048;
+			const u32 line = widthInQWords / height;
+			const u32 bpl = line << 3;
 
 			for (u32 y = 0; y < height; ++y) {
 				UnswapCopyWrap(RDRAM, address, (u8*)TMEM, tmemAddr << 3, 0xFFF, bpl);

commit 04b1f3f9f9dca0d8ad86746ea27098b1f9e53f44

Correct gDPLoadBlock line calculation again: fixed regression caused by commit f6be5fd39
Fixed regression in Racing Simulation 2 #167

src/gDP.cpp
@@ -626,7 +626,9 @@ void gDPLoadBlock(u32 tile, u32 uls, u32 ult, u32 lrs, u32 dxt)
 
 		if (dxt > 0) {
 			const u32 widthInQWords = (bytes >> 3);
-			const u32 height = (widthInQWords * dxt) / 2048;
+			u32 height = (widthInQWords * dxt) / 2048;
+			if ((widthInQWords * dxt) % 2048 >= 1024)
+				++height;
 			const u32 line = widthInQWords / height;
 			const u32 bpl = line << 3;
 