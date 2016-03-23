8488ebccd2a356da60a59c000cfd18ab0ef8a208
Sergey Lipskiy <gonetz@ngs.ru>
2016-03-23 10:22:25 +0600

    Fix texture coordinates calculation in vertex shader when TexturePersp disabled.
    Fixed Rogue Squadron Sky is broken [regression] #924

--- src/Combiner.cpp
+++ src/Combiner.cpp
@@ -366,7 +366,7 @@ Storage format:
   uint32 - number of shaders
   shaders in binary form
 */
-static const u32 ShaderStorageFormatVersion = 0x05U;
+static const u32 ShaderStorageFormatVersion = 0x06U;
 void CombinerInfo::_saveShadersStorage() const
 {
 	if (m_shadersLoaded >= m_combiners.size())
