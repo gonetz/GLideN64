bea7adf4b392b0514da639a894a46d17a034c88f
Sergey Lipskiy <gonetz@ngs.ru>
2016-03-29 22:21:52 +0600

    Add a hack for Jeremy McGrath Supercross 2000 jauge line, see #274
    Fixed issue #942

--- src/RSP.cpp
+++ src/RSP.cpp
@@ -343,6 +343,8 @@ void RSP_Init()
 	else if (strstr(RSP.romname, (const char *)"Perfect Dark") != NULL ||
 			 strstr(RSP.romname, (const char *)"PERFECT DARK") != NULL)
 		config.generalEmulation.hacks |= hack_rectDepthBufferCopyPD;
+	else if (strstr(RSP.romname, (const char *)"Jeremy McGrath Super") != NULL)
+		config.generalEmulation.hacks |= hack_ModifyVertexXyInShader;
 
 	api().FindPluginPath(RSP.pluginpath);
 
