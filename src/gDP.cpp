gonetz/GLideN64

commit bcde07a97822158e9ae3f5699b33936d6b6f4447

Don't skip buffer height calculation in gDPFillRectangle.
Fixed regression in Super Robot Spirits: Crash rankings option in HLE. #909

src/gDP.cpp
@@ -755,28 +755,25 @@ void gDPFillRectangle( s32 ulx, s32 uly, s32 lrx, s32 lry )
 		if (gDP.fillColor.color == DepthClearColor) {
 			gDPFillRDRAM(gDP.colorImage.address, ulx, uly, lrx, lry, gDP.colorImage.width, gDP.colorImage.size, gDP.fillColor.color);
 			render.clearDepthBuffer(uly, lry);
-			return;
 		}
 	} else if (gDP.fillColor.color == DepthClearColor && gDP.otherMode.cycleType == G_CYC_FILL) {
 		depthBufferList().saveBuffer(gDP.colorImage.address);
 		gDPFillRDRAM(gDP.colorImage.address, ulx, uly, lrx, lry, gDP.colorImage.width, gDP.colorImage.size, gDP.fillColor.color);
 		render.clearDepthBuffer(uly, lry);
-		return;
+	} else {
+		frameBufferList().setBufferChanged();
+		f32 fillColor[4];
+		gDPGetFillColor(fillColor);
+
+		if (gDP.otherMode.cycleType == G_CYC_FILL) {
+			if ((ulx == 0) && (uly == 0) && (lrx == gDP.scissor.lrx) && (lry == gDP.scissor.lry)) {
+				gDPFillRDRAM(gDP.colorImage.address, ulx, uly, lrx, lry, gDP.colorImage.width, gDP.colorImage.size, gDP.fillColor.color);
+				render.clearColorBuffer(fillColor);
+			} else
+				render.drawRect(ulx, uly, lrx, lry, fillColor);
+		} else
+			render.drawRect(ulx, uly, lrx, lry, fillColor);
 	}
-
-	frameBufferList().setBufferChanged();
-
-	f32 fillColor[4];
-	gDPGetFillColor(fillColor);
-	if (gDP.otherMode.cycleType == G_CYC_FILL) {
-		if ((ulx == 0) && (uly == 0) && (lrx == gDP.scissor.lrx) && (lry == gDP.scissor.lry)) {
-			gDPFillRDRAM(gDP.colorImage.address, ulx, uly, lrx, lry, gDP.colorImage.width, gDP.colorImage.size, gDP.fillColor.color);
-			render.clearColorBuffer(fillColor);
-			return;
-		}
-	}
-
-	render.drawRect(ulx, uly, lrx, lry, fillColor);
 
 	if (gDP.otherMode.cycleType == G_CYC_FILL) {
 		if (lry > (u32)gDP.scissor.lry)

commit 1db0c7a75e30ad0c932bf92ff29f5afb7db06f27

Fixed bug in gDPFillRectangle, caused by commit bcde07a9782

src/gDP.cpp
@@ -758,18 +758,23 @@ void gDPFillRectangle( s32 ulx, s32 uly, s32 lrx, s32 lry )
 	} else if (lry == uly)
 		++lry;
 
+	bool bClearBuffer = false;
 	if (gDP.depthImageAddress == gDP.colorImage.address) {
 		// Game may use depth texture as auxilary color texture. Example: Mario Tennis
 		// If color is not depth clear color, that is most likely the case
 		if (gDP.fillColor.color == DepthClearColor) {
 			gDPFillRDRAM(gDP.colorImage.address, ulx, uly, lrx, lry, gDP.colorImage.width, gDP.colorImage.size, gDP.fillColor.color);
 			render.clearDepthBuffer(uly, lry);
+			bClearBuffer = true;
 		}
 	} else if (gDP.fillColor.color == DepthClearColor && gDP.otherMode.cycleType == G_CYC_FILL) {
 		depthBufferList().saveBuffer(gDP.colorImage.address);
 		gDPFillRDRAM(gDP.colorImage.address, ulx, uly, lrx, lry, gDP.colorImage.width, gDP.colorImage.size, gDP.fillColor.color);
 		render.clearDepthBuffer(uly, lry);
-	} else {
+		bClearBuffer = true;
+	}
+
+	if (!bClearBuffer) {
 		frameBufferList().setBufferChanged();
 		f32 fillColor[4];
 		gDPGetFillColor(fillColor);