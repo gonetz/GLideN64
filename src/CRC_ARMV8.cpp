6181b0090c107a745e9b36e2d67855aa105c70ed
gizmo98 <atz.SE@gmx.de>
2016-09-04 11:08:48 +0200

    CRC.cpp: add ARMv8 crc intrinsics
    Speedup:
    CRC_Calculate() up to 11x
    CRC_CalculatePalette() up to 4x
    
    Will be only used if the right compiler flags are set.
    rpi3: -march=armv8-a+crc -mtune=cortex-a53
    
    http://infocenter.arm.com/help/index.jsp?topic=/com.arm.doc.dui0801f/pge
    1427897662265.html
    
    If CRC32 can be replaced with CRC32C SSE4.2 CRC32C intrinsics could be
    added as well.
    https://github.com/gonetz/GLideN64/pull/1056#issuecomment-244530166

---
+++ src/CRC_ARMV8.cpp
@@ -0,0 +1,51 @@
+#include "CRC.h"
+#include <arm_acle.h>
+
+void CRC_BuildTable()
+{
+}
+
+u32 CRC_Calculate( u32 crc, const void * buffer, u32 count )
+{
+	u8 *p;
+	u32 orig = crc;
+
+	p = (u8*) buffer;
+
+	// use eight byte crc intrinsic __crc32d if count is high enough.
+	// __crc32d, __crc32w, __crc32h and __crc32b use polynomial 0x04C11DB7
+	while (count >= 8) {
+		crc = __crc32d(crc, *((u64*)p));
+		p += 8;
+		count -= 8;
+	}
+	if (count >= 4) {
+		crc = __crc32w(crc, *((u32*)p));
+		p += 4;
+		count -= 4;
+	}
+	if (count >= 2) {
+		crc = __crc32h(crc, *((u16*)p));
+		p += 2;
+		count -= 2;
+	}
+	if (count == 1)
+		crc = __crc32b(crc, *p);
+
+	return crc ^ orig;
+}
+
+u32 CRC_CalculatePalette(u32 crc, const void * buffer, u32 count )
+{
+	u8 *p;
+	u32 orig = crc;
+
+	p = (u8*) buffer;
+	while (count--) {
+		// use two byte intrinsic __crc32h
+		crc = __crc32h(crc, *((u16*)p));
+		p += 8;
+	}
+
+	return crc ^ orig;
+}
