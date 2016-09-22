38ff9e97d5e72ab37616e7368ca48e67c156de77
Sergey Lipskiy <gonetz@ngs.ru>
2016-09-22 20:50:51 +0700

    Reset texture mode to normal in gSPSetSpriteTile.
    Fixed Nushi Zuri 64 incorrect gfx #977

--- src/gSP.cpp
+++ src/gSP.cpp
@@ -1825,6 +1825,8 @@ void gSPSetSpriteTile(const uObjSprite *_pObjSprite)
 	gDPSetTile( _pObjSprite->imageFmt, _pObjSprite->imageSiz, _pObjSprite->imageStride, _pObjSprite->imageAdrs, 0, _pObjSprite->imagePal, G_TX_CLAMP, G_TX_CLAMP, 0, 0, 0, 0 );
 	gDPSetTileSize( 0, 0, 0, (w - 1) << 2, (h - 1) << 2 );
 	gSPTexture( 1.0f, 1.0f, 0, 0, TRUE );
+	gSP.textureTile[0]->textureMode = TEXTUREMODE_NORMAL;
+	gSP.textureTile[1]->textureMode = TEXTUREMODE_NORMAL;
 }
 
 struct ObjData
