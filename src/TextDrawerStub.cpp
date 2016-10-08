5aae8076450b3acd50cb885ee1c989a4300febc7
Logan McNaughton <loganmc10@users.noreply.github.com>
2016-10-08 08:19:53 -0600

    Add getTextSize to TextDrawerStub

--- src/TextDrawerStub.cpp
+++ src/TextDrawerStub.cpp
@@ -4,6 +4,7 @@ TextDrawer::TextDrawer() {}
 void TextDrawer::init() {}
 void TextDrawer::destroy() {}
 void TextDrawer::renderText(const char * /*_pText*/, float /*x*/, float /*y*/) const {}
+void TextDrawer::getTextSize(const char *_pText, float & _w, float & _h) const {}
 TextDrawer & TextDrawer::get() {
 	static TextDrawer drawer;
 	return drawer;
