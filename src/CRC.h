92b39b6b725b7333152d467e5f04bef23cca2eaa
Sergey Lipskiy <gonetz@ngs.ru>
2016-10-13 14:34:33 +0700

    Correct typo in CRC.h

--- src/CRC.h
+++ src/CRC.h
@@ -2,7 +2,7 @@
 
 void CRC_Init();
 
-// Strict chacksum calculation. usually CRC32
+// Strict checksum calculation. usually CRC32
 u32 CRC_Calculate_Strict( u32 crc, const void *buffer, u32 count );
 u32 CRC_Calculate( u32 crc, const void *buffer, u32 count );
 u32 CRC_CalculatePalette( u32 crc, const void *buffer, u32 count );
