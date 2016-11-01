617517737d424ea77479aa7f2fbbca786d4d650c
Logan McNaughton <logan@bacoosta.com>
2016-11-01 14:57:40 -0700

    Specify a specific CRC function for microcodes

--- src/CRC32.cpp
+++ src/CRC32.cpp
@@ -44,6 +44,11 @@ u32 CRC_Calculate( u32 crc, const void * buffer, u32 count )
 	return crc ^ orig;
 }
 
+u32 CRC_Calculate_Strict( u32 crc, const void * buffer, u32 count )
+{
+	return CRC_Calculate(crc, buffer, count);
+}
+
 u32 CRC_CalculatePalette(u32 crc, const void * buffer, u32 count )
 {
 	u8 *p;
