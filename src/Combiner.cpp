5d7c02f7d11be277af625a630949d410aae79209
Sergey Lipskiy <gonetz@ngs.ru>
2016-11-14 20:53:55 +0700

    Up shader storage version.

--- src/Combiner.cpp
+++ src/Combiner.cpp
@@ -384,7 +384,7 @@ Storage format:
   uint32 - number of shaders
   shaders in binary form
 */
-static const u32 ShaderStorageFormatVersion = 0x0BU;
+static const u32 ShaderStorageFormatVersion = 0x0CU;
 void CombinerInfo::_saveShadersStorage() const
 {
 	if (m_shadersLoaded >= m_combiners.size())
