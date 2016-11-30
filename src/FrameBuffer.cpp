gonetz/GLideN64

commit f2b9883681406b01f4ffc1c88d0e04e2c3a35e10

Correct only dstY0 for lower field.
Fixed image jitter in topgear hyperbike

src/FrameBuffer.cpp
@@ -851,8 +851,8 @@ void FrameBufferList::renderBuffer(u32 _address)
 		Xoffset = 0;
 
 	if (isLowerField) {
-		if (srcY0 > 0)
-			--srcY0;
+//		if (srcY0 > 0)
+//			--srcY0;
 		if (dstY0 > 0)
 			--dstY0;
 	}

commit 807622f7bf107b18d2a7457913eaa1d1b9f25085

Fix image jitter in interlaced mode. Fixed regression caused by commit f2b9883

src/FrameBuffer.cpp
@@ -876,8 +876,10 @@ void FrameBufferList::renderBuffer(u32 _address)
 		Xoffset = 0;
 
 	if (isLowerField) {
-//		if (srcY0 > 0)
-//			--srcY0;
+		if (*REG.VI_WIDTH < VI.width * 2) {
+			if (srcY0 > 0)
+				--srcY0;
+		}
 		if (dstY0 > 0)
 			--dstY0;
 	}