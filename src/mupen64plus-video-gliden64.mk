1e8ae2aab2257efcf885982ddae1ef7e919f8650
Sergey Lipskiy <gonetz@ngs.ru>
2017-01-27 23:06:28 +0700

    Correct source files list in mupen64plus-video-gliden64.mk

--- src/mupen64plus-video-gliden64.mk
+++ src/mupen64plus-video-gliden64.mk
@@ -25,28 +25,29 @@ MY_LOCAL_C_INCLUDES :=                          \
 
 MY_LOCAL_SRC_FILES :=                               \
     $(SRCDIR)/Combiner.cpp                          \
+    $(SRCDIR)/CombinerKey.cpp                       \
     $(SRCDIR)/CommonPluginAPI.cpp                   \
     $(SRCDIR)/Config.cpp                            \
     $(SRCDIR)/convert.cpp                           \
     $(SRCDIR)/CRC32.cpp                             \
     $(SRCDIR)/DepthBuffer.cpp                       \
+    $(SRCDIR)/DisplayWindow.cpp                     \
     $(SRCDIR)/F3D.cpp                               \
-    $(SRCDIR)/F3DDKR.cpp                            \
-    $(SRCDIR)/F3DEX2CBFD.cpp                        \
-    $(SRCDIR)/F3DEX2.cpp                            \
-    $(SRCDIR)/F3DEX2MM.cpp                          \
-    $(SRCDIR)/F3DEX.cpp                             \
-    $(SRCDIR)/F3DPD.cpp                             \
-    $(SRCDIR)/F3DGOLDEN.cpp                         \
-    $(SRCDIR)/F3DSETA.cpp                           \
     $(SRCDIR)/F3DBETA.cpp                           \
-    $(SRCDIR)/FBOTextureFormats.cpp                 \
+    $(SRCDIR)/F3DDKR.cpp                            \
+    $(SRCDIR)/F3DEX.cpp                             \
+    $(SRCDIR)/F3DEX2.cpp                            \
+    $(SRCDIR)/F3DEX2CBFD.cpp                        \
+    $(SRCDIR)/F3DEX2MM.cpp                          \
+    $(SRCDIR)/F3DGOLDEN.cpp                         \
+    $(SRCDIR)/F3DPD.cpp                             \
+    $(SRCDIR)/F3DSETA.cpp                           \
     $(SRCDIR)/FrameBuffer.cpp                       \
     $(SRCDIR)/FrameBufferInfo.cpp                   \
     $(SRCDIR)/GBI.cpp                               \
     $(SRCDIR)/gDP.cpp                               \
     $(SRCDIR)/GLideN64.cpp                          \
-    $(SRCDIR)/glState.cpp                           \
+    $(SRCDIR)/GraphicsDrawer.cpp                    \
     $(SRCDIR)/gSP.cpp                               \
     $(SRCDIR)/Keys.cpp                              \
     $(SRCDIR)/L3D.cpp                               \
@@ -55,31 +56,53 @@ MY_LOCAL_SRC_FILES :=                               \
     $(SRCDIR)/Log_android.cpp                       \
     $(SRCDIR)/MupenPlusPluginAPI.cpp                \
     $(SRCDIR)/N64.cpp                               \
-    $(SRCDIR)/OpenGL.cpp                            \
+    $(SRCDIR)/NoiseTexture.cpp                      \
+    $(SRCDIR)/PaletteTexture.cpp                    \
     $(SRCDIR)/Performance.cpp                       \
     $(SRCDIR)/PostProcessor.cpp                     \
-    $(SRCDIR)/SoftwareRender.cpp                    \
     $(SRCDIR)/RDP.cpp                               \
     $(SRCDIR)/RSP.cpp                               \
     $(SRCDIR)/S2DEX2.cpp                            \
     $(SRCDIR)/S2DEX.cpp                             \
+    $(SRCDIR)/SoftwareRender.cpp                    \
     $(SRCDIR)/TextureFilterHandler.cpp              \
     $(SRCDIR)/Textures.cpp                          \
     $(SRCDIR)/Turbo3D.cpp                           \
     $(SRCDIR)/VI.cpp                                \
+    $(SRCDIR)/ZlutTexture.cpp                       \
     $(SRCDIR)/ZSort.cpp                             \
     $(SRCDIR)/ShaderUtils.cpp                       \
     $(SRCDIR)/common/CommonAPIImpl_common.cpp       \
     $(SRCDIR)/mupenplus/CommonAPIImpl_mupenplus.cpp \
     $(SRCDIR)/mupenplus/Config_mupenplus.cpp        \
     $(SRCDIR)/mupenplus/MupenPlusAPIImpl.cpp        \
-    $(SRCDIR)/mupenplus/OpenGL_mupenplus.cpp        \
     $(SRCDIR)/DepthBufferRender/ClipPolygon.cpp     \
     $(SRCDIR)/DepthBufferRender/DepthBufferRender.cpp     \
     $(SRCDIR)/BufferCopy/ColorBufferToRDRAM.cpp     \
     $(SRCDIR)/BufferCopy/DepthBufferToRDRAM.cpp     \
     $(SRCDIR)/BufferCopy/RDRAMtoColorBuffer.cpp     \
-    $(SRCDIR)/TextDrawer.cpp                        \
+    $(SRCDIR)/Graphics/Context.cpp                  \
+    $(SRCDIR)/Graphics/CombinerProgram.cpp          \
+    $(SRCDIR)/Graphics/OpenGLContext/GLFunctions.cpp                               \
+    $(SRCDIR)/Graphics/OpenGLContext/opengl_Attributes.cpp                         \
+    $(SRCDIR)/Graphics/OpenGLContext/opengl_BufferManipulationObjectFactory.cpp    \
+    $(SRCDIR)/Graphics/OpenGLContext/opengl_CachedFunctions.cpp                    \
+    $(SRCDIR)/Graphics/OpenGLContext/opengl_ColorBufferReaderWithBufferStorage.cpp \
+    $(SRCDIR)/Graphics/OpenGLContext/opengl_ColorBufferReaderWithPixelBuffer.cpp   \
+    $(SRCDIR)/Graphics/OpenGLContext/opengl_ColorBufferReaderWithEGLImage.cpp      \
+    $(SRCDIR)/Graphics/OpenGLContext/opengl_ContextImpl.cpp                        \
+    $(SRCDIR)/Graphics/OpenGLContext/opengl_GLInfo.cpp                             \
+    $(SRCDIR)/Graphics/OpenGLContext/opengl_Parameters.cpp                         \
+    $(SRCDIR)/Graphics/OpenGLContext/opengl_TextureManipulationObjectFactory.cpp   \
+    $(SRCDIR)/Graphics/OpenGLContext/opengl_UnbufferedDrawer.cpp                   \
+    $(SRCDIR)/Graphics/OpenGLContext/opengl_Utils.cpp                              \
+    $(SRCDIR)/Graphics/OpenGLContext/GLSL/glsl_CombinerInputs.cpp                  \
+    $(SRCDIR)/Graphics/OpenGLContext/GLSL/glsl_CombinerProgramBuilder.cpp          \
+    $(SRCDIR)/Graphics/OpenGLContext/GLSL/glsl_CombinerProgramImpl.cpp             \
+    $(SRCDIR)/Graphics/OpenGLContext/GLSL/glsl_CombinerProgramUniformFactory.cpp   \
+    $(SRCDIR)/Graphics/OpenGLContext/GLSL/glsl_ShaderStorage.cpp                   \
+    $(SRCDIR)/Graphics/OpenGLContext/GLSL/glsl_SpecialShadersFactory.cpp           \
+    $(SRCDIR)/Graphics/OpenGLContext/GLSL/glsl_Utils.cpp                           
 
 MY_LOCAL_CFLAGS :=      \
     $(COMMON_CFLAGS)    \
@@ -120,8 +143,7 @@ LOCAL_SHARED_LIBRARIES  := $(MY_LOCAL_SHARED_LIBRARIES) android-framework-ui
 LOCAL_STATIC_LIBRARIES  := $(MY_LOCAL_STATIC_LIBRARIES)
 LOCAL_ARM_MODE          := $(MY_LOCAL_ARM_MODE)
 LOCAL_C_INCLUDES        := $(MY_LOCAL_C_INCLUDES)
-LOCAL_SRC_FILES         := $(MY_LOCAL_SRC_FILES) $(SRCDIR)/GLUniforms/UniformSet.cpp $(SRCDIR)/GLES2/GLSLCombiner_gles2.cpp \
-                           $(SRCDIR)/BufferCopy/ColorBufferToRDRAM_GLES.cpp
+LOCAL_SRC_FILES         := $(MY_LOCAL_SRC_FILES)
 LOCAL_CFLAGS            := $(MY_LOCAL_CFLAGS) -DGLES2
 LOCAL_CPPFLAGS          := $(MY_LOCAL_CPPFLAGS)
 LOCAL_LDFLAGS           := $(MY_LOCAL_LDFLAGS)
@@ -137,8 +159,7 @@ LOCAL_SHARED_LIBRARIES  := $(MY_LOCAL_SHARED_LIBRARIES)
 LOCAL_STATIC_LIBRARIES  := $(MY_LOCAL_STATIC_LIBRARIES)
 LOCAL_ARM_MODE          := $(MY_LOCAL_ARM_MODE)
 LOCAL_C_INCLUDES        := $(MY_LOCAL_C_INCLUDES) $(LOCAL_PATH)/GLES3/include/
-LOCAL_SRC_FILES         := $(MY_LOCAL_SRC_FILES) $(SRCDIR)/GLUniforms/UniformSet.cpp $(SRCDIR)/OGL3X/GLSLCombiner_ogl3x.cpp \
-                           $(SRCDIR)/BufferCopy/ColorBufferToRDRAM_GL.cpp $(SRCDIR)/BufferCopy/ColorBufferToRDRAM_BufferStorageExt.cpp
+LOCAL_SRC_FILES         := $(MY_LOCAL_SRC_FILES)
 LOCAL_CFLAGS            := $(MY_LOCAL_CFLAGS) -DGLES3
 LOCAL_CPPFLAGS          := $(MY_LOCAL_CPPFLAGS)
 LOCAL_LDFLAGS           := $(MY_LOCAL_LDFLAGS)
@@ -154,8 +175,7 @@ LOCAL_SHARED_LIBRARIES  := $(MY_LOCAL_SHARED_LIBRARIES)
 LOCAL_STATIC_LIBRARIES  := $(MY_LOCAL_STATIC_LIBRARIES)
 LOCAL_ARM_MODE          := $(MY_LOCAL_ARM_MODE)
 LOCAL_C_INCLUDES        := $(MY_LOCAL_C_INCLUDES) $(LOCAL_PATH)/GLES3/include/
-LOCAL_SRC_FILES         := $(MY_LOCAL_SRC_FILES) $(SRCDIR)/GLUniforms/UniformSet.cpp $(SRCDIR)/OGL3X/GLSLCombiner_ogl3x.cpp \
-                           $(SRCDIR)/BufferCopy/ColorBufferToRDRAM_GL.cpp $(SRCDIR)/BufferCopy/ColorBufferToRDRAM_BufferStorageExt.cpp
+LOCAL_SRC_FILES         := $(MY_LOCAL_SRC_FILES)
 LOCAL_CFLAGS            := $(MY_LOCAL_CFLAGS) -DGLES3_1
 LOCAL_CPPFLAGS          := $(MY_LOCAL_CPPFLAGS)
 LOCAL_LDFLAGS           := $(MY_LOCAL_LDFLAGS)
@@ -171,8 +191,7 @@ LOCAL_SHARED_LIBRARIES  := $(MY_LOCAL_SHARED_LIBRARIES)
 LOCAL_STATIC_LIBRARIES  := $(MY_LOCAL_STATIC_LIBRARIES)
 LOCAL_ARM_MODE          := $(MY_LOCAL_ARM_MODE)
 LOCAL_C_INCLUDES        := $(MY_LOCAL_C_INCLUDES) $(LOCAL_PATH)/GL/
-LOCAL_SRC_FILES         := $(MY_LOCAL_SRC_FILES) $(SRCDIR)/GLUniforms/UniformSet.cpp $(SRCDIR)/OGL3X/GLSLCombiner_ogl3x.cpp \
-                           $(SRCDIR)/common/GLFunctions.cpp $(SRCDIR)/BufferCopy/ColorBufferToRDRAM_GL.cpp  $(SRCDIR)/BufferCopy/ColorBufferToRDRAM_BufferStorageExt.cpp
+LOCAL_SRC_FILES         := $(MY_LOCAL_SRC_FILES)
 LOCAL_CFLAGS            := $(MY_LOCAL_CFLAGS) -DEGL
 LOCAL_CPPFLAGS          := $(MY_LOCAL_CPPFLAGS)
 LOCAL_LDFLAGS           := $(MY_LOCAL_LDFLAGS)
