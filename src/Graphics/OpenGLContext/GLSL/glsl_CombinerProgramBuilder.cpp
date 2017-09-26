c925b8dd5447d34d201f1f62b020962f1ca58106
Logan <loganmc10@users.noreply.github.com>
2017-09-26 15:20:46 -0600

    Optimize 3point shader

--- src/Graphics/OpenGLContext/GLSL/glsl_CombinerProgramBuilder.cpp
+++ src/Graphics/OpenGLContext/GLSL/glsl_CombinerProgramBuilder.cpp
@@ -1478,15 +1478,11 @@ public:
 					"#define TEX_OFFSET(off) texture2D(tex, texCoord - (off)/texSize)			\n"
 					"lowp vec4 filter3point(in sampler2D tex, in mediump vec2 texCoord)			\n"
 					"{																			\n"
-#ifndef VC
-					"  mediump vec2 texSize = uTextureSize[nCurrentTile];						\n"
-#else
 					"  mediump vec2 texSize;													\n"
 					"  if (nCurrentTile == 0)													\n"
 					"    texSize = uTextureSize[0];												\n"
 					"  else																		\n"
 					"    texSize = uTextureSize[1];												\n"
-#endif
 					"  mediump vec2 offset = fract(texCoord*texSize - vec2(0.5));	\n"
 					"  offset -= step(1.0, offset.x + offset.y);								\n"
 					"  lowp vec4 c0 = TEX_OFFSET(offset);										\n"
