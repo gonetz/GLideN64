977fddf3ea9e0271a1280d94ad94dc7df0991eda
Sergey Lipskiy <gonetz@ngs.ru>
2017-10-11 15:06:32 +0700

    Correct condition for Zelda monochrome buffer.
    Fixed Pokemon Puzzle League: missing graphics in game (regression) #1588

--- src/Graphics/OpenGLContext/GLSL/glsl_CombinerProgramUniformFactory.cpp
+++ src/Graphics/OpenGLContext/GLSL/glsl_CombinerProgramUniformFactory.cpp
@@ -203,7 +203,10 @@ public:
 					nFbFixedAlpha0 = 1;
 			} else if (gSP.textureTile[0]->size == G_IM_SIZ_16b && gSP.textureTile[0]->format == G_IM_FMT_IA) {
 				nFbMonochromeMode0 = 2;
-			} else if (cache.current[0]->size == G_IM_SIZ_16b && gSP.textureTile[0]->size == G_IM_SIZ_8b && gSP.textureTile[0]->format == G_IM_FMT_CI) {
+			} else if ((config.generalEmulation.hacks & hack_ZeldaMM) != 0 &&
+					   cache.current[0]->size == G_IM_SIZ_16b &&
+					   gSP.textureTile[0]->size == G_IM_SIZ_8b &&
+					   gSP.textureTile[0]->format == G_IM_FMT_CI) {
 				// Zelda monochrome effect
 				nFbMonochromeMode0 = 3;
 				nFbMonochromeMode1 = 3;
