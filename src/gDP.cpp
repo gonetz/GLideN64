gonetz/GLideN64

commit b261f34b0d5cac11194a16788caa30d963af0e40

Fix gDP.loadTile->loadHeight calculation. Fixed Zelda 64 Skybox duplicates #2094.

src/gDP.cpp
@@ -499,9 +499,15 @@ void gDPLoadTile(u32 tile, u32 uls, u32 ult, u32 lrs, u32 lrt)
 
 	if (gDP.loadTile->masks == 0)
 		gDP.loadTile->loadWidth = max(gDP.loadTile->loadWidth, info.width);
-	if (gDP.loadTile->maskt == 0 && gDP.loadTile->tmem % gDP.loadTile->line == 0) {
-		const u16 theight = info.height + gDP.loadTile->tmem / gDP.loadTile->line;
-		gDP.loadTile->loadHeight = max(gDP.loadTile->loadHeight, theight);
+	if (gDP.loadTile->maskt == 0 &&
+		gDP.loadTile->tmem % gDP.loadTile->line == 0) {
+		u32 idx = 0;
+		while (gDP.tiles[idx].tmem != gDP.loadTile->tmem)
+			++idx;
+		if (idx == gSP.texture.tile) {
+			u16 theight = info.height + gDP.loadTile->tmem / gDP.loadTile->line;
+			gDP.loadTile->loadHeight = max(gDP.loadTile->loadHeight, theight);
+		}
 	}
 
 	u32 address = gDP.textureImage.address + gDP.loadTile->ult * gDP.textureImage.bpl + (gDP.loadTile->uls << gDP.textureImage.size >> 1);

commit 028133bb5c85bf3caae078d4a760f19a987e35e0

Correct gDP.loadTile->loadHeight calculation again. Fixed #2143 Yoshi's Story level backgrounds regression which was broken by b261f34b0

src/gDP.cpp
@@ -499,15 +499,12 @@ void gDPLoadTile(u32 tile, u32 uls, u32 ult, u32 lrs, u32 lrt)
 
 	if (gDP.loadTile->masks == 0)
 		gDP.loadTile->loadWidth = max(gDP.loadTile->loadWidth, info.width);
-	if (gDP.loadTile->maskt == 0 &&
-		gDP.loadTile->tmem % gDP.loadTile->line == 0) {
-		u32 idx = 0;
-		while (gDP.tiles[idx].tmem != gDP.loadTile->tmem)
-			++idx;
-		if (idx == gSP.texture.tile) {
+	if (gDP.loadTile->maskt == 0) {
+		if (gDP.otherMode.cycleType != G_CYC_2CYCLE && gDP.loadTile->tmem % gDP.loadTile->line == 0) {
 			u16 theight = info.height + gDP.loadTile->tmem / gDP.loadTile->line;
 			gDP.loadTile->loadHeight = max(gDP.loadTile->loadHeight, theight);
-		}
+		} else
+			gDP.loadTile->loadHeight = max(gDP.loadTile->loadHeight, info.height);
 	}
 
 	u32 address = gDP.textureImage.address + gDP.loadTile->ult * gDP.textureImage.bpl + (gDP.loadTile->uls << gDP.textureImage.size >> 1);