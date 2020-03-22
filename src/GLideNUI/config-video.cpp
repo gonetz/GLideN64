59822be52a38fc4c67409a8c24e8de184ea4e3f4
zilmar <zilmar@pj64-emu.com>
2020-03-22 16:00:57 +1030

    GlideNUI: implement CVideoTab::LoadSettings

--- src/GLideNUI/config-video.cpp
+++ src/GLideNUI/config-video.cpp
@@ -1,6 +1,31 @@
 #include "config-video.h"
 #include "FullscreenResolutions.h"
 #include "util.h"
+#include "../Config.h"
+
+static struct
+{
+    unsigned short width, height;
+    LPCTSTR description;
+}
+WindowedModes[] =
+{
+    { 320, 240, _T("320 x 240") },
+    { 400, 300, _T("400 x 300") },
+    { 480, 360, _T("480 x 360") },
+    { 640, 480, _T("640 x 480") },
+    { 800, 600, _T("800 x 600") },
+    { 960, 720, _T("960 x 720") },
+    { 1024, 768, _T("1024 x 768") },
+    { 1152, 864, _T("1152 x 864") },
+    { 1280, 960, _T("1280 x 960") },
+    { 1280, 1024, _T("1280 x 1024") },
+    { 1440, 1080, _T("1440 x 1080") },
+    { 1600, 1024, _T("1600 x 1024") },
+    { 1600, 1200, _T("1600 x 1200") }
+};
+static const unsigned int numWindowedModes = sizeof(WindowedModes) / sizeof(WindowedModes[0]);
+
 
 CVideoTab::CVideoTab() :
     CConfigTab(IDD_TAB_VIDEO)
@@ -148,4 +173,89 @@ void CVideoTab::ShowOverScanTab(int nTab)
 
 void CVideoTab::LoadSettings(bool /*blockCustomSettings*/)
 {
+    CComboBox WindowedResolutionComboBox(GetDlgItem(IDC_CMB_WINDOWED_RESOLUTION));
+    for (unsigned int i = 0; i < numWindowedModes; ++i)
+    {
+        int index = WindowedResolutionComboBox.AddString(WindowedModes[i].description);
+        WindowedResolutionComboBox.SetItemData(index, i);
+        if (WindowedModes[i].width == config.video.windowedWidth &&
+            WindowedModes[i].height == config.video.windowedHeight)
+        {
+            WindowedResolutionComboBox.SetCurSel(index);
+        }
+    }
+    if (WindowedResolutionComboBox.GetCount() > 0 && WindowedResolutionComboBox.GetCurSel() < 0)
+    {
+        WindowedResolutionComboBox.SetCurSel(0);
+    }
+
+    CButton overscanCheckBox(GetDlgItem(IDC_CHK_OVERSCAN));
+    overscanCheckBox.SetCheck(config.frameBufferEmulation.enableOverscan != 0 ? BST_CHECKED : BST_UNCHECKED);
+    OnOverscan(0, 0, NULL);
+
+    m_OverscanTabs[0]->SetValue(
+        config.frameBufferEmulation.overscanNTSC.left,
+        config.frameBufferEmulation.overscanNTSC.right,
+        config.frameBufferEmulation.overscanNTSC.top,
+        config.frameBufferEmulation.overscanNTSC.bottom
+    );
+
+    m_OverscanTabs[1]->SetValue(
+        config.frameBufferEmulation.overscanPAL.left,
+        config.frameBufferEmulation.overscanPAL.right,
+        config.frameBufferEmulation.overscanPAL.top,
+        config.frameBufferEmulation.overscanPAL.bottom
+    );
+
+    StringList fullscreenModesList, fullscreenRatesList;
+    int fullscreenMode, fullscreenRate;
+    fillFullscreenResolutionsList(fullscreenModesList, fullscreenMode, fullscreenRatesList, fullscreenRate);
+
+    CComboBox fullScreenResolutionComboBox(GetDlgItem(IDC_CMB_FULL_SCREEN_RES));
+    fullScreenResolutionComboBox.ResetContent();
+    for (size_t i = 0, n = fullscreenModesList.size(); i < n; i++)
+    {
+        std::wstring fullscreenModeStr(fullscreenModesList[i].begin(), fullscreenModesList[i].end());
+        int index = fullScreenResolutionComboBox.AddString(fullscreenModeStr.c_str());
+        if (fullscreenMode == i)
+        {
+            fullScreenResolutionComboBox.SetCurSel(index);
+        }
+    }
+    OnFullScreenChanged(0, 0, NULL);
+    m_AliasingSlider.SetPos(config.video.multisampling >> 1);
+    std::wstring AliasingText = FormatStrW(L"%dx", config.video.multisampling > 0 ? 1 << config.video.multisampling : 0);
+    CWindow(GetDlgItem(IDC_ALIASING_LABEL)).SetWindowTextW(AliasingText.c_str());
+
+    CButton(GetDlgItem(IDC_NOAA_RADIO)).SetCheck(config.video.multisampling == 0 ? BST_CHECKED : BST_UNCHECKED);
+    CButton(GetDlgItem(IDC_FXAA_RADIO)).SetCheck(config.video.fxaa != 0 && config.video.multisampling != 0 ? BST_CHECKED : BST_UNCHECKED);
+    CButton(GetDlgItem(IDC_MSAA_RADIO)).SetCheck(config.video.fxaa == 0 && config.video.multisampling != 0 ? BST_CHECKED : BST_UNCHECKED);
+    m_AnisotropicSlider.SetPos(config.texture.maxAnisotropy);
+    CWindow(GetDlgItem(IDC_ANISOTROPIC_LABEL)).SetWindowTextW(FormatStrW(L"%dx", m_AnisotropicSlider.GetPos()).c_str());
+
+    CButton(GetDlgItem(IDC_CHK_VERTICAL_SYNC)).SetCheck(config.video.verticalSync != 0 ? BST_CHECKED : BST_UNCHECKED);
+    CButton(GetDlgItem(IDC_CHK_THREADED_VIDEO)).SetCheck(config.video.threadedVideo != 0 ? BST_CHECKED : BST_UNCHECKED);
+
+    CButton(GetDlgItem(IDC_BILINEAR_3POINT)).SetCheck(config.texture.bilinearMode == BILINEAR_3POINT ? BST_CHECKED : BST_UNCHECKED);
+    CButton(GetDlgItem(IDC_BILINEAR_STANDARD)).SetCheck(config.texture.bilinearMode == BILINEAR_STANDARD ? BST_CHECKED : BST_UNCHECKED);
+
+    CButton(GetDlgItem(IDC_SCREENSHOT_PNG)).SetCheck(config.texture.screenShotFormat == 0 ? BST_CHECKED : BST_UNCHECKED);
+    CButton(GetDlgItem(IDC_SCREENSHOT_JPEG)).SetCheck(config.texture.screenShotFormat == 1 ? BST_CHECKED : BST_UNCHECKED);
+
+    CComboBox aspectComboBox(GetDlgItem(IDC_CMB_ASPECT_RATIO));
+    switch (config.frameBufferEmulation.aspect)
+    {
+    case Config::aStretch:
+        aspectComboBox.SetCurSel(2);
+        break;
+    case Config::a43:
+        aspectComboBox.SetCurSel(0);
+        break;
+    case Config::a169:
+        aspectComboBox.SetCurSel(1);
+        break;
+    case Config::aAdjust:
+        aspectComboBox.SetCurSel(3);
+        break;
+    }
 }
