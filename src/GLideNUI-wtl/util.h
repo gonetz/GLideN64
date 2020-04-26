93659a2207f6384beb13488234332f42a2a7e2b6
oddMLan <oddMLan2@gmail.com>
2020-04-26 02:07:13 -0700

    GLideNUI-wtl: Fix windowed resolution handling

--- src/GLideNUI-wtl/util.h
+++ src/GLideNUI-wtl/util.h
@@ -4,3 +4,8 @@
 std::string FromUTF16(const wchar_t * UTF16Source);
 std::wstring ToUTF16(const char * Source);
 std::wstring FormatStrW(const wchar_t * Source, ...);
+
+template <int min, int max> class range {
+public:
+	static bool contains(int i) { return min <= i && i < max; }
+};
