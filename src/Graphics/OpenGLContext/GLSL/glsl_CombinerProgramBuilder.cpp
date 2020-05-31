gonetz/GLideN64

commit a405ad86596d4ec33d84c67400212af3dd3ac236

Create Texture Engine unit

src/Graphics/OpenGLContext/GLSL/glsl_CombinerProgramBuilder.cpp
@@ -867,6 +867,8 @@ public:
 			"uniform lowp int uScreenSpaceTriangle;	\n"
 			"highp vec2 texCoord0;					\n"
 			"highp vec2 texCoord1;					\n"
+			"highp vec2[5] tcData0;					\n"
+			"highp vec2[5] tcData1;					\n"
 		;
 
 		if (config.generalEmulation.enableLegacyBlending != 0) {
@@ -1080,6 +1082,18 @@ public:
 			"	in highp vec4 vClamp, in highp vec2 vWrap,			\n"
 			"	in lowp vec2 vMirror, in highp vec2 vOffset);		\n"
 		;
+	}
+};
+
+class ShaderFragmentHeaderTextureEngine : public ShaderPart
+{
+public:
+	ShaderFragmentHeaderTextureEngine(const opengl::GLInfo & _glinfo)
+	{
+		m_part =
+			 "lowp vec2[5] textureEngine0(in highp vec2 texCoord); \n"
+			 "lowp vec2[5] textureEngine1(in highp vec2 texCoord); \n"
+			;
 	}
 };
 
@@ -2429,6 +2443,59 @@ public:
 	}
 };
 
+class ShaderTextureEngine : public ShaderPart
+{
+public:
+	ShaderTextureEngine(const opengl::GLInfo _glinfo) {
+		m_part =
+
+			"highp vec2[5] textureEngine0(in highp vec2 texCoord) \n"
+			"{  \n"
+			"  highp vec2[5] tcData; \n" // {tc00, tc01, tc10, tc11, frPart}
+			"  mediump vec2 intPart = floor(texCoord); \n"
+			"  tcData[0] = clampWrapMirror(intPart, uTexWrap0, uTexClamp0, uTexClampEn0, uTexMirrorEn0); \n"
+			"  tcData[3] = clampWrapMirror(intPart + vec2(1.0,1.0), uTexWrap0, uTexClamp0, uTexClampEn0, uTexMirrorEn0); \n"
+			"  tcData[1] = vec2(tcData[0].s, tcData[3].t); \n"
+			"  tcData[2] = vec2(tcData[3].s, tcData[0].t); \n"
+			"  tcData[4] = texCoord - intPart; \n"
+			"  return tcData;"
+			"}  \n"
+			"highp vec2[5] textureEngine1(in highp vec2 texCoord) \n"
+			"{  \n"
+			"  highp vec2[5] tcData; \n" // {tc00, tc01, tc10, tc11, frPart}
+			"  mediump vec2 intPart = floor(texCoord); \n"
+			"  tcData[0] = clampWrapMirror(intPart, uTexWrap1, uTexClamp1, uTexClampEn1, uTexMirrorEn1); \n"
+			"  tcData[3] = clampWrapMirror(intPart + vec2(1.0,1.0), uTexWrap1, uTexClamp1, uTexClampEn1, uTexMirrorEn1); \n"
+			"  tcData[1] = vec2(tcData[0].s, tcData[3].t); \n"
+			"  tcData[2] = vec2(tcData[3].s, tcData[0].t); \n"
+			"  tcData[4] = texCoord - intPart; \n"
+			"  return tcData;"
+			"}  \n"
+			;
+	}
+};
+
+
+class ShaderFragmentTextureEngineTex0 : public ShaderPart {
+public:
+	ShaderFragmentTextureEngineTex0(const opengl::GLInfo _glinfo)
+	{
+		m_part =
+			"tcData0 = textureEngine0(vTexCoord0); \n"
+			;
+	}
+};
+
+class ShaderFragmentTextureEngineTex1 : public ShaderPart {
+public:
+	ShaderFragmentTextureEngineTex1(const opengl::GLInfo _glinfo)
+	{
+		m_part =
+			"tcData1 = textureEngine1(vTexCoord1); \n"
+			;
+	}
+};
+
 /*---------------ShaderPartsEnd-------------*/
 
 static
@@ -2598,6 +2665,7 @@ graphics::CombinerProgram * CombinerProgramBuilder::buildCombinerProgram(Combine
 		m_fragmentHeaderDepthCompare->write(ssShader);
 		m_fragmentHeaderReadMSTex->write(ssShader);
 		m_fragmentHeaderClampWrapMirror->write(ssShader);
+		m_fragmentHeaderTextureEngine->write(ssShader);
 		if (bUseLod)
 			m_fragmentHeaderMipMap->write(ssShader);
 		else if (g_cycleType < G_CYC_COPY)
@@ -2631,9 +2699,15 @@ graphics::CombinerProgram * CombinerProgramBuilder::buildCombinerProgram(Combine
 
 	if (bUseTextures) {
 		if (combinerInputs.usesTile(0))
+		{
 			m_fragmentClampWrapMirrorTex0->write(ssShader);
+			m_fragmentTextureEngineTex0->write(ssShader);
+		}
 		if (combinerInputs.usesTile(1))
+		{
 			m_fragmentClampWrapMirrorTex1->write(ssShader);
+			m_fragmentTextureEngineTex1->write(ssShader);
+		}
 
 		if (bUseLod) {
 			m_fragmentReadTexMipmap->write(ssShader);
@@ -2673,6 +2747,7 @@ graphics::CombinerProgram * CombinerProgramBuilder::buildCombinerProgram(Combine
 
 	if (bUseTextures) {
 		m_shaderClampWrapMirror->write(ssShader);
+		m_shaderTextureEngine->write(ssShader);
 		if (bUseLod)
 			m_shaderMipmap->write(ssShader);
 		else {
@@ -2784,6 +2859,7 @@ CombinerProgramBuilder::CombinerProgramBuilder(const opengl::GLInfo & _glinfo, o
 , m_fragmentHeaderCalcLight(new ShaderFragmentHeaderCalcLight(_glinfo))
 , m_fragmentHeaderMipMap(new ShaderFragmentHeaderMipMap(_glinfo))
 , m_fragmentHeaderClampWrapMirror(new ShaderFragmentHeaderClampWrapMirror(_glinfo))
+, m_fragmentHeaderTextureEngine(new ShaderFragmentHeaderTextureEngine(_glinfo))
 , m_fragmentHeaderReadMSTex(new ShaderFragmentHeaderReadMSTex(_glinfo))
 , m_fragmentHeaderDither(new ShaderFragmentHeaderDither(_glinfo))
 , m_fragmentHeaderDepthCompare(new ShaderFragmentHeaderDepthCompare(_glinfo))
@@ -2796,6 +2872,8 @@ CombinerProgramBuilder::CombinerProgramBuilder(const opengl::GLInfo & _glinfo, o
 , m_fragmentReadTex1(new ShaderFragmentReadTex1(_glinfo))
 , m_fragmentClampWrapMirrorTex0(new ShaderFragmentClampWrapMirrorTex0(_glinfo))
 , m_fragmentClampWrapMirrorTex1(new ShaderFragmentClampWrapMirrorTex1(_glinfo))
+, m_fragmentTextureEngineTex0(new ShaderFragmentTextureEngineTex0(_glinfo))
+, m_fragmentTextureEngineTex1(new ShaderFragmentTextureEngineTex1(_glinfo))
 , m_fragmentReadTexCopyMode(new ShaderFragmentReadTexCopyMode(_glinfo))
 , m_fragmentReadTexMipmap(new ShaderFragmentReadTexMipmap(_glinfo))
 , m_fragmentCallN64Depth(new ShaderFragmentCallN64Depth(_glinfo))
@@ -2811,6 +2889,7 @@ CombinerProgramBuilder::CombinerProgramBuilder(const opengl::GLInfo & _glinfo, o
 , m_shaderN64DepthCompare(new ShaderN64DepthCompare(_glinfo))
 , m_shaderN64DepthRender(new ShaderN64DepthRender(_glinfo))
 , m_shaderClampWrapMirror(new ShaderClampWrapMirror(_glinfo))
+, m_shaderTextureEngine(new ShaderTextureEngine(_glinfo))
 , m_useProgram(_useProgram)
 , m_combinerOptionsBits(graphics::CombinerProgram::getShaderCombinerOptionsBits())
 {

src/Graphics/OpenGLContext/GLSL/glsl_CombinerProgramBuilder.h
@@ -63,6 +63,7 @@ namespace glsl {
 		ShaderPartPtr m_fragmentHeaderCalcLight;
 		ShaderPartPtr m_fragmentHeaderMipMap;
 		ShaderPartPtr m_fragmentHeaderClampWrapMirror;
+		ShaderPartPtr m_fragmentHeaderTextureEngine;
 		ShaderPartPtr m_fragmentHeaderReadMSTex;
 		ShaderPartPtr m_fragmentHeaderDither;
 		ShaderPartPtr m_fragmentHeaderDepthCompare;
@@ -75,6 +76,8 @@ namespace glsl {
 		ShaderPartPtr m_fragmentReadTex1;
 		ShaderPartPtr m_fragmentClampWrapMirrorTex0;
 		ShaderPartPtr m_fragmentClampWrapMirrorTex1;
+		ShaderPartPtr m_fragmentTextureEngineTex0;
+		ShaderPartPtr m_fragmentTextureEngineTex1;
 		ShaderPartPtr m_fragmentReadTexCopyMode;
 		ShaderPartPtr m_fragmentReadTexMipmap;
 		ShaderPartPtr m_fragmentCallN64Depth;
@@ -91,6 +94,7 @@ namespace glsl {
 		ShaderPartPtr m_shaderN64DepthCompare;
 		ShaderPartPtr m_shaderN64DepthRender;
 		ShaderPartPtr m_shaderClampWrapMirror;
+		ShaderPartPtr m_shaderTextureEngine;
 
 		std::unique_ptr<CombinerProgramUniformFactory> m_uniformFactory;
 

commit 4f58d66e960a575a35ef3d783cca25fd71f5bc25

Fix regressions in GLES 2.0 support introduced by a405ad8 - IOS compiler also doesn't like the vec2 as return Type, thus it's made a out param

src/Graphics/OpenGLContext/GLSL/glsl_CombinerProgramBuilder.cpp
@@ -869,8 +869,8 @@ public:
 			"uniform lowp int uScreenSpaceTriangle;	\n"
 			"highp vec2 texCoord0;					\n"
 			"highp vec2 texCoord1;					\n"
-			"highp vec2[5] tcData0;					\n"
-			"highp vec2[5] tcData1;					\n"
+			"highp vec2 tcData0[5];					\n"
+			"highp vec2 tcData1[5];					\n"
 		;
 
 		if (config.generalEmulation.enableLegacyBlending != 0) {
@@ -1083,8 +1083,8 @@ public:
 			"highp vec2 clampWrapMirror(in highp vec2 vTexCoord,	\n"
 			"	in highp vec2 vWrap, in highp vec2 vClamp,			\n"
 			"	in lowp vec2 vClampEn, in lowp vec2 vMirrorEn );	\n"
-			"highp vec2[5] textureEngine0(in highp vec2 texCoord);	\n"
-			"highp vec2[5] textureEngine1(in highp vec2 texCoord);	\n"
+			"void textureEngine0(in highp vec2 texCoord, out highp vec2 tcData[5]); \n"
+			"void textureEngine1(in highp vec2 texCoord, out highp vec2 tcData[5]); \n"
 			;
 	}
 };
@@ -1103,7 +1103,7 @@ public:
 			(g_cycleType == G_CYC_COPY || g_textureConvert.useTextureFiltering()))
 		{
 			shader <<
-				"lowp vec4 readTexMS(in lowp sampler2DMS mstex, in highp vec2[5] tcData, in lowp int fbMonochrome, in lowp int fbFixedAlpha);\n";
+				"lowp vec4 readTexMS(in lowp sampler2DMS mstex, in highp vec2 tcData[5], in lowp int fbMonochrome, in lowp int fbFixedAlpha);\n";
 		}
 	}
 
@@ -1305,7 +1305,7 @@ public:
 			if (g_textureConvert.useTextureFiltering()) {
 				shaderPart +=
 					"uniform lowp int uTextureFilterMode;								\n"
-					"lowp vec4 readTex(in sampler2D tex, in highp vec2[5] tcData, in lowp int fbMonochrome, in lowp int fbFixedAlpha);	\n"
+					"lowp vec4 readTex(in sampler2D tex, in highp vec2 tcData[5], in lowp int fbMonochrome, in lowp int fbFixedAlpha);	\n"
 					;
 			}
 			if (g_textureConvert.useYUVCoversion()) {
@@ -1313,7 +1313,7 @@ public:
 					"uniform lowp ivec2 uTextureFormat;									\n"
 					"uniform lowp int uTextureConvert;									\n"
 					"uniform mediump ivec4 uConvertParams;								\n"
-					"lowp vec4 YUV_Convert(in sampler2D tex, in highp vec2[5] tcData, in lowp int convert, in lowp int format, in lowp vec4 prev);	\n"
+					"lowp vec4 YUV_Convert(in sampler2D tex, in highp vec2 tcData[5], in lowp int convert, in lowp int format, in lowp vec4 prev);	\n"
 					;
 			}
 		}
@@ -1352,7 +1352,7 @@ public:
 				;
 		} else {
 			m_part =
-				"lowp vec4 readTex(in sampler2D tex, in highp vec2[5] tcData, in lowp int fbMonochrome, in lowp int fbFixedAlpha);	\n"
+				"lowp vec4 readTex(in sampler2D tex, in highp vec2 tcData[5], in lowp int fbMonochrome, in lowp int fbFixedAlpha);	\n"
 			;
 		}
 	}
@@ -1821,8 +1821,9 @@ public:
 	ShaderMipmap(const opengl::GLInfo & _glinfo)
 	{
 		if (_glinfo.isGLES2) {
-			"uniform mediump vec2 uTextureSize[2];										\n"
-				"lowp vec4 TextureMipMap(in sampler2D tex, in highp vec2[5] tcData, in lowp float lod)	\n"
+			m_part =
+				"uniform mediump vec2 uTextureSize[2];										\n"
+				"lowp vec4 TextureMipMap(in sampler2D tex, in highp vec2 tcData[5], in lowp float lod)	\n"
 				"{																					\n"
 				"  mediump vec2 texSize;															\n"
 				"  if (nCurrentTile == 0)															\n"
@@ -1840,7 +1841,7 @@ public:
 				;
 			if (config.generalEmulation.enableLOD == 0) {
 				// Fake mipmap
-				m_part =
+				m_part +=
 					"uniform lowp int uMaxTile;			\n"
 					"uniform mediump float uMinLod;		\n"
 					"														\n"
@@ -1852,7 +1853,7 @@ public:
 					"}														\n"
 				;
 			} else {
-				m_part =
+				m_part +=
 					"uniform lowp int uEnableLod;		\n"
 					"uniform mediump float uMinLod;		\n"
 					"uniform lowp int uMaxTile;			\n"
@@ -2069,7 +2070,7 @@ public:
 		if (m_glinfo.isGLES2) {
 			shaderPart +=
 				"uniform mediump vec2 uTextureSize[2];										\n"
-				"lowp vec4 TextureNearest(in sampler2D tex, in highp vec2[5] tcData)		\n"
+				"lowp vec4 TextureNearest(in sampler2D tex, in highp vec2 tcData[5])		\n"
 				"{																					\n"
 				"  mediump vec2 texSize;															\n"
 				"  if (nCurrentTile == 0)															\n"
@@ -2081,7 +2082,7 @@ public:
 				;
 			if (g_textureConvert.useYUVCoversion())
 				shaderPart +=
-				"lowp vec4 YUV_Convert(in sampler2D tex, in highp vec2[5] tcData, in lowp int convert, in lowp int format, in lowp vec4 prev)	\n"
+				"lowp vec4 YUV_Convert(in sampler2D tex, in highp vec2 tcData[5], in lowp int convert, in lowp int format, in lowp vec4 prev)	\n"
 				"{																	\n"
 				"  lowp vec4 texColor;												\n"
 				"  if (convert != 0) texColor = prev;								\n"
@@ -2104,7 +2105,7 @@ public:
 						// 3 point texture filtering.
 						// Original author: ArthurCarvalho
 						// GLSL implementation: twinaphex, mupen64plus-libretro project.
-						"lowp vec4 TextureFilter(in sampler2D tex, in highp vec2[5] tcData)		\n"
+						"lowp vec4 TextureFilter(in sampler2D tex, in highp vec2 tcData[5])		\n"
 						"{																					\n"
 						"  mediump vec2 texSize;															\n"
 						"  if (nCurrentTile == 0)															\n"
@@ -2125,7 +2126,7 @@ public:
 					shaderPart +=
 						// bilinear filtering.
 						//"uniform mediump vec2 uTextureSize[2];										\n" NOT NEEDED HERE?
-						"lowp vec4 TextureFilter(in sampler2D tex, in highp vec2[5] tcData)		\n"
+						"lowp vec4 TextureFilter(in sampler2D tex, in highp vec2 tcData[5])		\n"
 						"{																					\n"
 						"  mediump vec2 texSize;															\n"
 						"  if (nCurrentTile == 0)															\n"
@@ -2143,7 +2144,7 @@ public:
 						;
 				}
 				shaderPart +=
-					"lowp vec4 readTex(in sampler2D tex, in highp vec2[5] tcData, in lowp int fbMonochrome, in lowp int fbFixedAlpha)	\n"
+					"lowp vec4 readTex(in sampler2D tex, in highp vec2 tcData[5], in lowp int fbMonochrome, in lowp int fbFixedAlpha)	\n"
 					"{																			\n"
 					"  lowp vec4 texColor;														\n"
 					"  if (uTextureFilterMode == 0) texColor = TextureNearest(tex, tcData);		\n"
@@ -2168,7 +2169,7 @@ public:
 					"  return texel / float(uMSAASamples);										\n"
 					"}																			\n"
 					"																			\n"
-					"lowp vec4 readTexMS(in lowp sampler2DMS mstex, in highp vec2[5] tcData, in lowp int fbMonochrome, in lowp int fbFixedAlpha)	\n"
+					"lowp vec4 readTexMS(in lowp sampler2DMS mstex, in highp vec2 tcData[5], in lowp int fbMonochrome, in lowp int fbFixedAlpha)	\n"
 					"{																			\n"
 					"  mediump ivec2 itexCoord;													\n"
 					"  if (fbMonochrome == 3) {													\n"
@@ -2206,7 +2207,7 @@ public:
 		if (_glinfo.isGLES2) {
 			m_part =
 				"uniform mediump vec2 uTextureSize[2];										\n"
-				"lowp vec4 TextureNearest(in sampler2D tex, in highp vec2[5] tcData)		\n"
+				"lowp vec4 TextureNearest(in sampler2D tex, in highp vec2 tcData[5])		\n"
 				"{																					\n"
 				"  mediump vec2 texSize;															\n"
 				"  if (nCurrentTile == 0)															\n"
@@ -2215,7 +2216,7 @@ public:
 				"    texSize = uTextureSize[1];														\n"
 				"  return texture2D(tex, (tcData[0] + 0.5) / texSize);								\n"
 				"  }																				\n"
-				"lowp vec4 readTex(in sampler2D tex, in highp vec2[5] tcData, in lowp int fbMonochrome, in lowp int fbFixedAlpha)	\n"
+				"lowp vec4 readTex(in sampler2D tex, in highp vec2 tcData[5], in lowp int fbMonochrome, in lowp int fbFixedAlpha)	\n"
 				"{																			\n"
 				"  lowp vec4 texColor = TextureNearest(tex, tcData);						\n"
 				"  if (fbMonochrome == 1) texColor = vec4(texColor.r);						\n"
@@ -2237,7 +2238,7 @@ public:
 					"  return texel / float(uMSAASamples);										\n"
 					"}																			\n"
 					"																			\n"
-					"lowp vec4 readTexMS(in lowp sampler2DMS mstex, in highp vec2[5] tcData, in lowp int fbMonochrome, in lowp int fbFixedAlpha)	\n"
+					"lowp vec4 readTexMS(in lowp sampler2DMS mstex, in highp vec2 tcData[5], in lowp int fbMonochrome, in lowp int fbFixedAlpha)	\n"
 					"{																			\n"
 					"  mediump ivec2 itexCoord;													\n"
 					"  if (fbMonochrome == 3) {													\n"
@@ -2406,9 +2407,8 @@ public:
 			"}																				\n"
 
 			"uniform highp vec2 uTexSize0;		\n"
-			"highp vec2[5] textureEngine0(in highp vec2 texCoord) \n"
+			"void textureEngine0(in highp vec2 texCoord, out highp vec2 tcData[5]) \n"
 			"{  \n"
-			"  highp vec2[5] tcData; \n" // {tc00, tc01, tc10, tc11, frPart}
 			"  mediump vec2 intPart = floor(texCoord); \n"
 			"  highp vec2 tc00 = clampWrapMirror(intPart, uTexWrap0, uTexClamp0, uTexWrapEn0, uTexClampEn0, uTexMirrorEn0); \n"
 			"  highp vec2 tc11 = clampWrapMirror(intPart + vec2(1.0,1.0), uTexWrap0, uTexClamp0, uTexWrapEn0, uTexClampEn0, uTexMirrorEn0); \n"
@@ -2417,13 +2417,11 @@ public:
 			"  tcData[1] = vec2(tcData[0].s, tcData[3].t); \n"
 			"  tcData[2] = vec2(tcData[3].s, tcData[0].t); \n"
 			"  tcData[4] = texCoord - intPart; \n"
-			"  return tcData;"
 			"}  \n"
 
 			"uniform highp vec2 uTexSize1;		\n"
-			"highp vec2[5] textureEngine1(in highp vec2 texCoord) \n"
+			"void textureEngine1(in highp vec2 texCoord, out highp vec2 tcData[5]) \n"
 			"{  \n"
-			"  highp vec2[5] tcData; \n" // {tc00, tc01, tc10, tc11, frPart}
 			"  mediump vec2 intPart = floor(texCoord); \n"
 			"  highp vec2 tc00 = clampWrapMirror(intPart, uTexWrap1, uTexClamp1, uTexWrapEn1, uTexClampEn1, uTexMirrorEn1); \n"
 			"  highp vec2 tc11 = clampWrapMirror(intPart + vec2(1.0,1.0), uTexWrap1, uTexClamp1, uTexWrapEn1, uTexClampEn1, uTexMirrorEn1); \n"
@@ -2432,7 +2430,6 @@ public:
 			"  tcData[1] = vec2(tcData[0].s, tcData[3].t); \n"
 			"  tcData[2] = vec2(tcData[3].s, tcData[0].t); \n"
 			"  tcData[4] = texCoord - intPart; \n"
-			"  return tcData;"
 			"}  \n"
 
 			;
@@ -2445,7 +2442,7 @@ public:
 	ShaderFragmentTextureEngineTex0(const opengl::GLInfo _glinfo)
 	{
 		m_part =
-			"tcData0 = textureEngine0(vTexCoord0); \n"
+			"textureEngine0(vTexCoord0, tcData0); \n"
 			;
 	}
 };
@@ -2455,7 +2452,7 @@ public:
 	ShaderFragmentTextureEngineTex1(const opengl::GLInfo _glinfo)
 	{
 		m_part =
-			"tcData1 = textureEngine1(vTexCoord1); \n"
+			"textureEngine1(vTexCoord1, tcData1); \n"
 			;
 	}
 };