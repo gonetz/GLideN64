gonetz/GLideN64

commit 156171bebda74e5c3e5ab8f568e702dfa9a9c457

Set viewport in TextDrawer::drawText
Fixed text messages not shown in some games.

src/TextDrawer.cpp
@@ -346,6 +346,9 @@ void TextDrawer::drawText(const char *_pText, float _x, float _y) const
 	gfxContext.setBlending(blend::SRC_ALPHA, blend::ONE_MINUS_SRC_ALPHA);
 	m_program->activate();
 
+	gfxContext.setViewport(0, 0, wnd.getWidth(), wnd.getHeight());
+	gSP.changed |= CHANGED_VIEWPORT;
+
 	Context::TexParameters setParams;
 	setParams.handle = m_atlas->m_pTexture->name;
 	setParams.textureUnitIndex = textureIndices::Tex[0];

commit 047fa63b2af39f6cd9f0f2f4f24eb69b6c1717a1

Fix OSD info in fullscreen mode. It was broken by commit 156171be.

src/TextDrawer.cpp
@@ -346,7 +346,8 @@ void TextDrawer::drawText(const char *_pText, float _x, float _y) const
 	gfxContext.setBlending(blend::SRC_ALPHA, blend::ONE_MINUS_SRC_ALPHA);
 	m_program->activate();
 
-	gfxContext.setViewport(0, 0, wnd.getWidth(), wnd.getHeight());
+	gfxContext.setViewport((wnd.getScreenWidth() - wnd.getWidth()) / 2, (wnd.getScreenHeight() - wnd.getHeight()) / 2,
+		wnd.getWidth(), wnd.getHeight());
 	gSP.changed |= CHANGED_VIEWPORT;
 
 	Context::TexParameters setParams;