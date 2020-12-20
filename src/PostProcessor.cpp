2be9e0fe59cd43cc431f6af27a29c9b46850a7da
fzurita <fzurita@gmail.com>
2020-12-19 19:18:38 -0500

    Fix issues with post processing and scaling changes

--- src/PostProcessor.cpp
+++ src/PostProcessor.cpp
@@ -95,7 +95,8 @@ PostProcessor & PostProcessor::get()
 
 void PostProcessor::_preDraw(FrameBuffer * _pBuffer)
 {
-	if (!m_pResultBuffer || m_pResultBuffer->m_width != _pBuffer->m_width)
+	if (!m_pResultBuffer || m_pResultBuffer->m_width != _pBuffer->m_width ||
+		m_pResultBuffer->m_scale != _pBuffer->m_scale)
 		_createResultBuffer(_pBuffer);
 
 	if (_pBuffer->m_pTexture->frameBufferTexture == CachedTexture::fbMultiSample) {
