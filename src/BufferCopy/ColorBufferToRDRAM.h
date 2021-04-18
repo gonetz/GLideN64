43a5d16905c0c30baf5f1f773362e2dba9e3ab1d
fzurita <fzurita@gmail.com>
2021-04-17 20:45:47 -0400

    Keep a color copy FBO and texture per N64 frame buffer

--- src/BufferCopy/ColorBufferToRDRAM.h
+++ src/BufferCopy/ColorBufferToRDRAM.h
@@ -29,18 +29,12 @@ private:
 	ColorBufferToRDRAM(const ColorBufferToRDRAM &) = delete;
 	virtual ~ColorBufferToRDRAM();
 
-	CachedTexture * m_pTexture;
-
 	union RGBA {
 		struct {
 			u8 r, g, b, a;
 		};
 		u32 raw;
 	};
-
-	void _initFBTexture(void);
-
-	void _destroyFBTexure(void);
 
 	bool _prepareCopy(u32& _startAddress);
 
@@ -51,17 +45,11 @@ private:
 	static u16 _RGBAtoRGBA16(u32 _c, u32 x, u32 y);
 	static u32 _RGBAtoRGBA32(u32 _c, u32 x, u32 y);
 
-	graphics::ObjectHandle m_FBO;
 	FrameBuffer * m_pCurFrameBuffer;
 	u32 m_frameCount;
 	u32 m_startAddress;
 
-	u32 m_lastBufferWidth;
-
 	static u32 m_blueNoiseIdx;
-
-	std::array<u32, 3> m_allowedRealWidths;
-	std::unique_ptr<graphics::ColorBufferReader> m_bufferReader;
 };
 
 void copyWhiteToRDRAM(FrameBuffer * _pBuffer);
