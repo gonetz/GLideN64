70a9213d69cbb823fc0ec30c8a990ff68f6c25fe
Rosalie Wanders <rosalie@mailbox.org>
2021-08-10 23:09:20 +0200

    ScreenShot: move to common windows implementation

---
+++ src/windows/ScreenShot.h
@@ -0,0 +1,6 @@
+#ifndef SCREENSHOT_H
+#define SCREENSHOT_H
+
+void SaveScreenshot(const wchar_t * _folder, const char * _name, int _width, int _height, const unsigned char * _data);
+
+#endif // SCREENSHOT_H
