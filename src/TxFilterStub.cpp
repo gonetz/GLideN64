d328ea50c746162d263ef1a013080bfcdbfdb44c
Sergey Lipskiy <gonetz@ngs.ru>
2021-10-30 13:02:13 +0700

    Correct include in TxFilterStub.cpp Follow up commit 71788c

--- src/TxFilterStub.cpp
+++ src/TxFilterStub.cpp
@@ -1,4 +1,4 @@
-#include "GLideNHQ/Ext_TxFilter.h"
+#include "GLideNHQ/TxFilterExport.h"
 
 TAPI boolean TAPIENTRY
 txfilter_init(int maxwidth, int maxheight, int maxbpp, int options, int cachesize,
@@ -9,7 +9,7 @@ txfilter_init(int maxwidth, int maxheight, int maxbpp, int options, int cachesiz
 }
 
 TAPI void TAPIENTRY
-txfilter_shutdown(void) 
+txfilter_shutdown(void)
 {}
 
 TAPI boolean TAPIENTRY
