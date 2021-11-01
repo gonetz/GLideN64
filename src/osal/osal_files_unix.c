f39ce6494b67e7992587b8bea98c673745a8a60e
Rosalie <rosalie@mailbox.org>
2021-11-01 18:17:36 +0100

    osal_files_unix: fix osal_search_dir_read_next crash

--- src/osal/osal_files_unix.c
+++ src/osal/osal_files_unix.c
@@ -133,6 +133,9 @@ EXPORT const wchar_t * CALL osal_search_dir_read_next(void * dir_handle)
     DIR *dir = (DIR *) dir_handle;
     struct dirent *entry;
 
+    if (dir_handle == NULL)
+        return NULL;
+
     entry = readdir(dir);
     if (entry == NULL)
         return NULL;
