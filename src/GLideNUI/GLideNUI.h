698f5580249e2edf86fb89cd75d921535171074c
Rosalie Wanders <rosalie@mailbox.org>
2023-02-10 23:03:07 +0100

    GLideNUI: improve GLideN64.custom.ini loading

--- src/GLideNUI/GLideNUI.h
+++ src/GLideNUI/GLideNUI.h
@@ -13,15 +13,10 @@ extern "C" {
 #define CALL
 #endif
 
-#ifdef M64P_GLIDENUI
-EXPORT bool CALL IsPathWriteable(const wchar_t * dir);
-EXPORT void CALL CopyConfigFiles(const wchar_t * _srcDir, const wchar_t * _targetDir);
-#endif // M64P_GLIDENUI
-
-EXPORT bool CALL RunConfig(const wchar_t * _strFileName, const char * _romName, unsigned int _maxMSAALevel, unsigned int _maxAnisotropy);
+EXPORT bool CALL RunConfig(const wchar_t * _strFileName, const wchar_t * _strSharedFileName, const char * _romName, unsigned int _maxMSAALevel, unsigned int _maxAnisotropy);
 EXPORT int CALL RunAbout(const wchar_t * _strFileName);
-EXPORT void CALL LoadConfig(const wchar_t * _strFileName);
-EXPORT void CALL LoadCustomRomSettings(const wchar_t * _strFileName, const char * _romName);
+EXPORT void CALL LoadConfig(const wchar_t * _strFileName, const wchar_t * _strSharedFileName);
+EXPORT void CALL LoadCustomRomSettings(const wchar_t * _strFileName, const wchar_t * _strSharedFileName, const char * _romName);
 
 #if defined(__cplusplus)
 }
--- src/GLideNUI/Settings.h
+++ src/GLideNUI/Settings.h
@@ -1,21 +1,17 @@
 #ifndef SETTINGS_H
 #define SETTINGS_H
 
-void loadSettings(const QString & _strIniFolder);
+void loadSettings(const QString & _strIniFolder, const QString & _strSharedIniFolder);
 void writeSettings(const QString & _strIniFolder);
 void resetSettings(const QString & _strIniFolder);
-void loadCustomRomSettings(const QString & _strIniFolder, const char * _strRomName);
-void saveCustomRomSettings(const QString & _strIniFolder, const char * _strRomName);
+void loadCustomRomSettings(const QString & _strIniFolder, const QString & _strSharedIniFolder, const char * _strRomName);
+void saveCustomRomSettings(const QString & _strIniFolder, const QString & _strSharedIniFolder, const char * _strRomName);
 QString getTranslationFile();
 QStringList getProfiles(const QString & _strIniFolder);
 QString getCurrentProfile(const QString & _strIniFolder);
-void changeProfile(const QString & _strIniFolder, const QString & _strProfile);
+void changeProfile(const QString & _strIniFolder, const QString & _strSharedIniFolder, const QString & _strProfile);
 void addProfile(const QString & _strIniFolder, const QString & _strProfile);
 void removeProfile(const QString & _strIniFolder, const QString & _strProfile);
-#ifdef M64P_GLIDENUI
-bool isPathWriteable(const QString dir);
-void copyConfigFiles(const QString _srcDir, const QString _targetDir);
-#endif // M64P_GLIDENUI
 
 #endif // SETTINGS_H
 
