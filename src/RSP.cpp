1f4d04f43b53739bc9b128ab5577d20e3d60ed6a
Sergey Lipskiy <gonetz@ngs.ru>
2023-02-24 18:40:42 +0700

    Add a hack to fix Paper Mario subscreen delay: small increase of Blue component of the color buffer does the trick.

--- src/RSP.cpp
+++ src/RSP.cpp
@@ -350,6 +350,9 @@ void RSP_Init()
 	else if (strstr(RSP.romname, (const char *)"Extreme G 2") != nullptr ||
 		strstr(RSP.romname, (const char *)"\xb4\xb8\xbd\xc4\xd8\xb0\xd1\x47\x32") != nullptr)
 		config.generalEmulation.hacks |= hack_noDepthFrameBuffers;
+	else if (strstr(RSP.romname, (const char *)"PAPER MARIO") ||
+		strstr(RSP.romname, (const char *)"MARIO STORY"))
+		config.generalEmulation.hacks |= hack_paper_mario_subscreen;
 
 	api().FindPluginPath(RSP.pluginpath);
 
