f9da98c06a67113cb7734892b4a67707aca7bbff
Squall-Leonhart <danialhorton@hotmail.com>
2024-09-08 23:34:38 +1000

    Fix issue #2772
    When building, the Config initialization was being inherited in a busted way from GlidNUI instead of GlideNUI-wtl, leading to a character of the rom name being interpretted as the current profile (I think)
    
    This changes fixes the ability to read/write custom and user profile settings.
    
    It also turns enables dependencies for GlideNUI and -wtl so they are built before GlideN64, preventing cases on windows where the libs for those weren't already built causing build failure.

--- src/GLideNUI-wtl/GLideNUI.h
+++ src/GLideNUI-wtl/GLideNUI.h
@@ -13,10 +13,10 @@ extern "C" {
 #define CALL
 #endif
 
-EXPORT bool CALL RunConfig(const wchar_t * _strFileName, const char * _romName, unsigned int _maxMSAALevel, unsigned int _maxAnisotropy);
-EXPORT int CALL RunAbout(const wchar_t * _strFileName);
-EXPORT void CALL LoadConfig(const wchar_t * _strFileName);
-EXPORT void CALL LoadCustomRomSettings(const wchar_t * _strFileName, const char * _romName);
+EXPORT bool CALL RunConfig(const wchar_t * _strFileName, const wchar_t * _strSharedFileName, const char * _romName, unsigned int _maxMSAALevel, unsigned int _maxAnisotropy);
+    EXPORT int CALL RunAbout(const wchar_t * _strFileName);
+EXPORT void CALL LoadConfig(const wchar_t * _strFileName, const wchar_t * _strSharedFileName);
+    EXPORT void CALL LoadCustomRomSettings(const wchar_t * _strFileName, const wchar_t * _strSharedFileName, const char * _romName);
 
 #if defined(__cplusplus)
 }
