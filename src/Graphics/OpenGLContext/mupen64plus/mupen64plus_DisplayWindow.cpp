b8c4824dfd18584cc2d9ecb6d59e68f477d7f8f6
Morilli <35152647+Morilli@users.noreply.github.com>
2025-08-31 07:43:41 +0200

    adjust logic to match _readScreen

--- src/Graphics/OpenGLContext/mupen64plus/mupen64plus_DisplayWindow.cpp
+++ src/Graphics/OpenGLContext/mupen64plus/mupen64plus_DisplayWindow.cpp
@@ -253,18 +253,20 @@ void DisplayWindowMupen64plus::_readScreen2(void * _dest, int * _width, int * _h
 		return;
 
 #if !defined(OS_ANDROID) && !defined(OS_IOS)
-	GLint oldFbo;
 	GLint oldMode;
-	glGetIntegerv(GL_READ_FRAMEBUFFER_BINDING, &oldFbo);
-	glBindFramebuffer(GL_READ_FRAMEBUFFER, static_cast<GLuint>(graphics::ObjectHandle::defaultFramebuffer));
 	glGetIntegerv(GL_READ_BUFFER, &oldMode);
+	gfxContext.bindFramebuffer(graphics::bufferTarget::READ_FRAMEBUFFER, graphics::ObjectHandle::defaultFramebuffer);
 	if (_front != 0)
 		glReadBuffer(GL_FRONT);
 	else
 		glReadBuffer(GL_BACK);
 	glReadPixels(0, m_heightOffset, m_screenWidth, m_screenHeight, GL_RGB, GL_UNSIGNED_BYTE, _dest);
+	if (graphics::BufferAttachmentParam(oldMode) == graphics::bufferAttachment::COLOR_ATTACHMENT0) {
+		FrameBuffer * pBuffer = frameBufferList().getCurrent();
+		if (pBuffer != nullptr)
+			gfxContext.bindFramebuffer(graphics::bufferTarget::READ_FRAMEBUFFER, pBuffer->m_FBO);
+	}
 	glReadBuffer(oldMode);
-	glBindFramebuffer(GL_READ_FRAMEBUFFER, oldFbo);
 #else
 	u8 *pBufferData = (u8*)malloc((*_width)*(*_height) * 4);
 	if (pBufferData == nullptr)
