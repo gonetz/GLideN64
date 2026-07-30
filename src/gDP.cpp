635aa2dbec486b6e62f67797c563d00046763849
aglab2 <aglab3@gmail.com>
2026-07-30 21:22:12 +0800

    Fixed incorrect gSPMemset emulation
    Fixes boot for Zeldarune romhack that uses F3DEX3 and gSPMemset for ZBuffer.
    
    1) Remove RDRAM address bits to satisfy physical address comparisons.
    2) Restore attached fb + attach depth buffer.

--- src/gDP.cpp
+++ src/gDP.cpp
@@ -831,6 +831,7 @@ void gDPMemset(u32 value, u32 addr, u32 length)
 		calculateParams(gDP.depthImageAddress, G_IM_SIZ_16b, imageWidth);
 
 		// HACK: this usually replaces gDPSetColorImage for zb so save zb
+		const auto backupCurr = frameBufferList().getCurrent();
 		frameBufferList().saveBuffer(gDP.depthImageAddress, (u16)G_IM_FMT_RGBA, (u16)G_IM_SIZ_16b, (u16)imageWidth, false);
 
 		if (config.generalEmulation.enableFragmentDepthWrite == 0)
@@ -847,6 +848,11 @@ void gDPMemset(u32 value, u32 addr, u32 length)
 			ValueKeeper<gDPInfo::OtherMode> backupOtherMode(gDP.otherMode, otherMode);
 			drawer.drawRect(0, uly, imageWidth, lry);
 			frameBufferList().setBufferChanged(f32(lry));
+		}
+
+		if (backupCurr != nullptr) {
+			frameBufferList().setCurrent(backupCurr);
+			frameBufferList().attachDepthBuffer();
 		}
 	}
 	else if (0 == config.frameBufferEmulation.enable) {
